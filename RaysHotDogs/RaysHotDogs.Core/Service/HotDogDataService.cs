c7a954b76168e6e58b6586816b0270dc3d34a5b5
Kevin Gagnon <klg73@outlook.com>
2018-06-22 14:13:37 -0400

    Added the shared code for the HotDog program

---
+++ RaysHotDogs/RaysHotDogs.Core/Service/HotDogDataService.cs
@@ -0,0 +1,41 @@
+﻿using RaysHotDogs.Core.Models;
+using RaysHotDogs.Core.Repository;
+using System.Collections.Generic;
+
+namespace RaysHotDogs.Core.Service
+{
+    public class HotDogDataService
+    {
+        private static HotDogRepository hotDogRepository = new HotDogRepository();
+
+        public HotDogDataService()
+        {
+        }
+
+        public List<HotDog> GetAllHotDogs()
+        {
+            return hotDogRepository.GetAllHotDogs();
+        }
+
+        public List<HotDogGroup> GetGroupedHotDogs()
+        {
+            return hotDogRepository.GetGroupedHotDogs();
+        }
+
+        public List<HotDog> GetHotDogsForGroup(int hotDogGroupId)
+        {
+            return hotDogRepository.GetHotDogsForGroup(hotDogGroupId);
+        }
+
+        public List<HotDog> GetFavoriteHotDogs()
+        {
+            return hotDogRepository.GetFavoriteHotDogs();
+        }
+
+        public HotDog GetHotDogById(int hotDogId)
+        {
+            return hotDogRepository.GetHotDogById(hotDogId);
+        }
+
+    }
+}
